49286e93fc713298a670d9bd14bdf72d34f1d32f
matthewfernst <matthew.f.ernst@gmail.com>
2022-08-06 22:13:51 -0700

    prettier rc

--- electron/main.ts
+++ electron/main.ts
@@ -6,63 +6,62 @@ import { RelayWiring } from "../src/vite-env";
 let mainWindow;
 
 const garageSwitch = (
-	event: any,
-	relayWiring: RelayWiring = "forward",
-	relayPin: number = 4
+    event: any,
+    relayWiring: RelayWiring = "forward",
+    relayPin: number = 4
 ): boolean => {
-    
-	const relayOn = 1;
-	const relayOff = 0;
-	const relayDirection = relayWiring === "forward";
+    const relayOn = 1;
+    const relayOff = 0;
+    const relayDirection = relayWiring === "forward";
 
-	try {
-		const doorPin = new Gpio(relayPin, "out");
-		doorPin.writeSync(relayDirection ? relayOff : relayOn);
+    try {
+        const doorPin = new Gpio(relayPin, "out");
+        doorPin.writeSync(relayDirection ? relayOff : relayOn);
 
-		const flipSwitch = () => {
-			doorPin.writeSync(relayDirection ? relayOn : relayOff);
-			setTimeout(() => doorPin.writeSync(relayDirection ? relayOff : relayOn), 500);
-		};
+        const flipSwitch = () => {
+            doorPin.writeSync(relayDirection ? relayOn : relayOff);
+            setTimeout(() => doorPin.writeSync(relayDirection ? relayOff : relayOn), 500);
+        };
 
-		flipSwitch();
-		return true;
-	} catch (err) {
-		console.log("Unsupported Platform Or Unknown Error Occured");
-		return false;
-	}
+        flipSwitch();
+        return true;
+    } catch (err) {
+        console.log("Unsupported Platform Or Unknown Error Occured");
+        return false;
+    }
 };
 
 function createWindow() {
-	mainWindow = new BrowserWindow({
-		width: 1300,
-		height: 750,
-		show: false,
-		darkTheme: true,
-		fullscreen: process.env.NODE_ENV === "production",
-		autoHideMenuBar: true,
-		webPreferences: {
-			nodeIntegration: true,
-			preload: path.join(__dirname, "preload.js")
-		}
-	});
+    mainWindow = new BrowserWindow({
+        width: 1300,
+        height: 750,
+        show: false,
+        darkTheme: true,
+        fullscreen: process.env.NODE_ENV === "production",
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: true,
+            preload: path.join(__dirname, "preload.js")
+        }
+    });
 
-	let indexPath = "http://localhost:3000";
-	if (process.env.NODE_ENV === "production") {
-		indexPath = "file://" + path.join(__dirname, "..", "index.html");
-	}
+    let indexPath = "http://localhost:3000";
+    if (process.env.NODE_ENV === "production") {
+        indexPath = "file://" + path.join(__dirname, "..", "index.html");
+    }
 
-	mainWindow.loadURL(indexPath);
-	mainWindow.once("ready-to-show", () => mainWindow.show());
-	mainWindow.on("closed", () => (mainWindow = null));
+    mainWindow.loadURL(indexPath);
+    mainWindow.once("ready-to-show", () => mainWindow.show());
+    mainWindow.on("closed", () => (mainWindow = null));
 }
 
 app.on("ready", () => {
-	ipcMain.handle("garageSwitch", garageSwitch);
-	createWindow();
+    ipcMain.handle("garageSwitch", garageSwitch);
+    createWindow();
 });
 // For sunrise and sunset times API. No SSL Certificate error.
 app.commandLine.appendSwitch("ignore-certificate-errors");
 app.on("window-all-closed", () => app.quit());
 app.on("activate", () => {
-	if (mainWindow === null) createWindow();
+    if (mainWindow === null) createWindow();
 });
--- electron/preload.ts
+++ electron/preload.ts
@@ -2,6 +2,6 @@ import { contextBridge, ipcRenderer } from "electron";
 import { RelayWiring } from "../src/vite-env";
 
 contextBridge.exposeInMainWorld("garageAPI", {
-	garageSwitch: (relayWiring: RelayWiring, relayPin: number) =>
-		ipcRenderer.invoke("garageSwitch", relayWiring, relayPin)
+    garageSwitch: (relayWiring: RelayWiring, relayPin: number) =>
+        ipcRenderer.invoke("garageSwitch", relayWiring, relayPin)
 });
