7dff4945bab3d3d682165fdca0fbce1ffae0d8e9
Max Rosoff <me@maxrosoff.com>
2022-10-29 21:09:47 -0700

    Fix Electron Plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,21 @@ import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react";
 import electron from "vite-plugin-electron";
+import renderer from "vite-plugin-electron-renderer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         electron({
-            entry: ["electron/main.ts", "electron/preload.ts"]
+            entry: ["electron/main.ts", "electron/preload.ts"],
         }),
-        react()
+        react(),
+        renderer({
+            nodeIntegration: true
+        })
     ],
     server: {
         port: 3000,
-        strictPort: true
+        strictPort: true,
     }
 });
