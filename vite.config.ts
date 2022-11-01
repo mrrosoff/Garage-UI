c7545e10b867abaaf5041209e72058d10a75a2a5
Matthew Ernst <matthew.f.ernst@gmail.com>
2022-11-01 14:10:18 -0700

    vite test
    Signed-off-by: Matthew Ernst <matthew.f.ernst@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import renderer from "vite-plugin-electron-renderer";
 export default defineConfig({
     build: {
         target: "esnext",
-        sourcemap: true,
+        sourcemap: false,
     },
     plugins: [
         electron({
