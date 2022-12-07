544341a64dd3fb4f1b279e9646bd17955498d5e6
matthewfernst <matthew.f.ernst@gmail.com>
2022-12-06 21:53:21 -0800

    iframe options updated
    Signed-off-by: matthewfernst <matthew.f.ernst@gmail.com>

--- src/components/Cards/MountainInfoCard.tsx
+++ src/components/Cards/MountainInfoCard.tsx
@@ -260,9 +260,9 @@ const LiveStreamModal = (props: any) => {
                         width="100%"
                         height="100%"
                         src={VITE_YOUTUBE_LIVE_STREAM_LINK + "?autoplay=1"}
-                        frameborder="0"
+                        frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen
+                        allowFullScreen
                     ></iframe>
                 </DialogContent>
             </Dialog>
