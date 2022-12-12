b546ecf448a81137c97101821fb6c63e70bc0d99
matthewfernst <matthew.f.ernst@gmail.com>
2022-12-12 12:14:09 -0800

    close alerts
    Signed-off-by: matthewfernst <matthew.f.ernst@gmail.com>

--- src/components/Cards/SnowPatrolInfoCard.tsx
+++ src/components/Cards/SnowPatrolInfoCard.tsx
@@ -1,9 +1,11 @@
 import { Alert, AlertTitle, Box } from "@mui/material";
+import { useState } from "react";
 
 import callExternalAPIOnInterval from "../../hooks/callExternalAPIOnInterval";
 
 const SnowPatrolInfoCard = () => {
     const { VITE_TIME_INTERVAL, VITE_SKI_RESORT_ID } = import.meta.env;
+    const [showAlert, setShowAlert] = useState(true);
     const resortData = callExternalAPIOnInterval(
         VITE_TIME_INTERVAL,
         `https://mtnpowder.com/feed?resortId=${VITE_SKI_RESORT_ID}`
@@ -15,20 +17,38 @@ const SnowPatrolInfoCard = () => {
         return null;
     }
 
+    const handleClose = () => {
+        setShowAlert(false);
+    };
     return (
         <Box width={"30%"}>
-            {snowPatrolAlert === "--" ? (
-                <Alert severity="info">
-                    <AlertTitle>Snow Patrol Information</AlertTitle>
-                    {snowPatrolReport}
-                </Alert>
+            {snowPatrolAlert === "--" && showAlert ? (
+                <AlertToMake
+                    severity="info"
+                    title="Snow Patrol Information"
+                    message={snowPatrolReport}
+                    handleClose={handleClose}
+                />
             ) : (
-                <Alert severity="error">
-                    <AlertTitle>Important Alert From Snow Patrol</AlertTitle>
-                    {snowPatrolAlert}
-                </Alert>
+                showAlert && (
+                    <AlertToMake
+                        severity="error"
+                        title="Important Alert From Snow Patrol"
+                        message={snowPatrolAlert}
+                        handleClose={handleClose}
+                    />
+                )
             )}
         </Box>
+    );
+};
+
+const AlertToMake = (props: any) => {
+    return (
+        <Alert severity={props.severity} onClose={props.handleClose}>
+            <AlertTitle>{props.title}</AlertTitle>
+            {props.message}
+        </Alert>
     );
 };
 
